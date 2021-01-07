9bfcc29c4f055e2bb83464f472e86e8df87de474
Eddwan Hallen <eddwan@gmail.com>
2021-01-07 16:02:53 -0300

    modified about page and added the resume in the index

--- pages/about.js
+++ pages/about.js
@@ -52,88 +52,30 @@ export default function About() {
         </h1>
         <div className="mb-8 prose leading-6 text-gray-600 dark:text-gray-400">
           <p>
-            Hey, I’m Lee. I'm a developer, writer, and the creator of&nbsp;
-            <a
-              href="https://masteringnextjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Mastering Next.js
-            </a>
-            &nbsp;and&nbsp;
-            <a
-              href="https://react2025.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React 2025.
-            </a>
-            &nbsp;I work at ▲Vercel as a Solutions Architect.
+            I have a hobby and it is called programming. I like to think how to
+            program everything and every time I got myself think what code could
+            I write to resolve something. I started my fascination by computers
+            when I was 9.
           </p>
           <p>
-            I’ve spoken across the country at conferences and meet-ups about
-            front-end development, design, and recruiting. I write about
-            development, tech careers, and my personal life on&nbsp;
-            <Link href="/newsletter">
-              <a>my newsletter.</a>
-            </Link>
+            Today I have degree in Internet and Computers Networking Technology
+            and a post degree in MBA of Project Management. Acting as Consulting
+            Engineer for Collaboration Technology sector since 2008 with a huge
+            knowledge about Cisco products, specially for Collaboration sector.
+            Few years ago I started this blog/page, but I had no chance to
+            update it.
           </p>
           <p>
-            I grew up in small-town Iowa and went to school at Iowa State,
-            graduating with a degree in Computer Engineering. I spend my free
-            time playing music, creating videos, and enjoying time with friends
-            and family in Des Moines, IA.
+            My plan is, through this blog, share a little bit of my knowledge
+            and help others to find some answers I was looking for in the past.
+            Here I want to share code snippets, ideas, programming, networking
+            and collaboration projects deployments.
+          </p>
+          <p>
+            Let’s see how I can update this page and share different knowledges
+            here.
           </p>
         </div>
-        <h2 className="font-bold text-3xl tracking-tight mb-4 text-black dark:text-white">
-          Conference Talks
-        </h2>
-        <Talk
-          title="Data Fetching with Next.js"
-          link="https://www.youtube.com/watch?v=Sz7SImkdIpo&feature=youtu.be&t=6063"
-        >
-          Next.js is a hybrid React framework allowing you to choose your data
-          fetching strategy on a per-page basis. In this talk, you’ll learn more
-          about CSR (Client-Side Rendering), SSR (Server-Side Rendering), SSG
-          (Static-Site Generation), ISR (Incremental Static Regeneration), and
-          when to use each one. I’ll also give a quick introduction to Next.js
-          and share some of the main features of the framework.
-        </Talk>
-        <Talk
-          title="Building Static Sites with Prisma and Next.js"
-          link="https://youtu.be/z3rmZbOEBCM?t=273"
-        >
-          During this workshop, you'll learn how to integrate Prisma with
-          Next.js and build a statically-generated site that displays a list of
-          your favorite songs. We'll use Chakra UI for styling and deploy our
-          site with Vercel.
-        </Talk>
-        <Talk
-          title="Building Component Libraries with a Monorepo"
-          link="/building-component-libraries-with-a-monorepo.pdf"
-        >
-          Learn why your organization needs a component library and discover the
-          best practices for building, scaling, and adopting it across all
-          platforms. We'll be using industry-standard technology (React,
-          JavaScript, Storybook) alongside cutting-edge solutions (CSS-in-JS,
-          Monorepo).
-        </Talk>
-        <Talk
-          title="Recruiting Engineers (From An Engineer's Perspective)"
-          link="/recruiting-engineers-talent42-lee-robinson.pdf"
-        >
-          Hiring talent is becoming increasingly difficult with low unemployment
-          rates and the tech industry booming. What you can do to stick out?
-          Learn from an engineer who's been involved on both sides - both as a
-          candidate and with hiring - on what candidates really want out of a
-          position.
-        </Talk>
-        <iframe
-          height="280"
-          src="https://www.google.com/maps/d/embed?mid=1QOGi-u8d4vwoQ4vC4zQjKxrSfsDIQdOK&hl=en"
-          title="Lee's Travel Map"
-          width="100%"
-        />
       </div>
     </Container>
   );
--- pages/index.js
+++ pages/index.js
@@ -28,12 +28,14 @@ export default function Home({ posts }) {
             the internet – everything you want to know and more is here.
           </h2>
           <div>
-            <button
-              type="button"
-              className="w-full inline-flex justify-center border border-transparent shadow-sm px-4 py-2 bg-red-500 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-            >
-              Download Resumé
-            </button>
+            <NextLink href="/Resume-Eddwan-Hallen.pdf">
+              <button
+                type="button"
+                className="w-full inline-flex justify-center border border-transparent shadow-sm px-4 py-2 bg-red-500 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Download Resumé
+              </button>
+            </NextLink>
           </div>
         </div>
       </div>
